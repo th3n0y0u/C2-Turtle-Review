8022b445bdef71a5227f94fe9c56b418bb9a4d8a
soop <>
2021-04-12 22:29:39 +0000

    code

--- main.py
+++ main.py
@@ -15,8 +15,48 @@ variable2 = turtle.Screen()
 
 4. movements
   a. forward
+  variable1.forward(DISTANCE)
+  variable.fd(DISTANCE)
+
   b. backward
+  variable1.backward(DISTANCE)
+  variable.bk(DISTANCE)
+
   c. turn right
+  variable1.right(ANGLE)
+
   d. turn left
+  variable.left(ANGLE)
+
+5. pen features
+  penup
+  variable1.penup()
+
+  pendown
+  variable1.pendown()
+
+  move to a point
+  variable1.goto(x, y)
+
+  move to the origin
+  variable1.home()
+
+  color
+  variable1.color(rgb)
+
+  shape(arrow, turtle, circle, square)
+  variable1.shape("SHAPE")
+
+  speed
+  variable1.speed()
+
+  write
+  variable1.write(TEXT, MOVE, ALIGN, FONT)
+
+  width(1,10)
+  variable1.width()
+  
+  circle
+  variable.circle(RADIUS)
 
 '''
