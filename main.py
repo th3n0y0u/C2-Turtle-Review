0960842cc436e492ccc279e70f3dac1f2289c1c7
Do Bryan <>
2021-04-12 23:28:33 +0000

    more code

--- main.py
+++ main.py
@@ -59,4 +59,40 @@ variable2 = turtle.Screen()
   circle
   variable.circle(RADIUS)
 
+  fill color
+  variable1.fill("COLOR")
+  variable1.begin_fill()
+  # SHAPE
+  variable1.end_fill()
+
 '''
+import turtle
+
+artist = turtle.Turtle()
+artist.shape("turtle")
+artist.speed(3)
+
+# Exercise 1: Drawing a star
+def drawStar(l):
+  for i in range(5):
+    artist.forward(l) 
+    artist.right(144) 
+
+drawStar(int(input("LENGTH: ")))
+
+# Exercise 2: Draw a Shape with circle()
+def drawShape(r, s):
+  for i in range(s):
+    artist.circle(r, 360, s)
+  
+drawShape(float(input("Radius:")), int(input("Number of Sides:")))
+
+# Exercise 3: Draw a Spiral
+colors = ["yellow", "blue", "green", "orange", "red",  "pink"]
+def drawSpiral():
+  for i in range(40):
+    artist.color(colors[i % 6])
+    artist.width(i / 5 + 1)
+    artist.forward(i)
+    artist.left(20)
+drawSpiral()
