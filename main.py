89fe2590e476714d21e4eb1ef47643910490517b
soop <>
2021-04-12 22:12:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+4/12/2021
+
+Review
+
+1. import turtle libary
+import turtle # top of page
+
+2. declare turtle
+variable1 = turtle.turtle()
+
+3. declare screen(option)
+variable2 = turtle.Screen()
+
+4. movements
+  a. forward
+  b. backward
+  c. turn right
+  d. turn left
+
+'''
